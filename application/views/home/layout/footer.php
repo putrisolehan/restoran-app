6c97fae666185b1bf4fa56f7fda84a0d486b2740
Putri Soleha <putsoleha@gmail.com>
2026-04-29 19:40:11 +0700

    last update

--- application/views/home/layout/footer.php
+++ application/views/home/layout/footer.php
@@ -9,8 +9,7 @@
         <a href="https://facebook.com/<?= $facebook ?>" class="facebook" target="_blank"><i class="bx bxl-facebook"></i></a>
       </div>
       <div class="copyright">
-        &copy; Copyright <strong><span>Bjn Squad</span></strong>. All Rights Reserved | Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a>
-				
+        &copy; 2026 Developed as a Group Project
       </div>
     </div>
   </footer><!-- End Footer -->
